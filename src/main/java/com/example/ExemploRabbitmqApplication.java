bc0545d2783e33cdefd89c4927001306b85cd6ce
lincoln <lincoln.ufop@gmail.com>
2019-06-22 10:56:36 -0300

    Commit inicial

---
+++ src/main/java/com/example/ExemploRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploRabbitmqApplication.class, args);
+	}
+
+}
