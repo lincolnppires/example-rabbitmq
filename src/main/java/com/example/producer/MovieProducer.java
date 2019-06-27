8712081f7841cb88f196ba54bdb07849c6acc92f
lincoln <lincoln.ufop@gmail.com>
2019-06-27 07:38:07 -0300

    Produtor para exchange movie.drama.work

--- src/main/java/com/example/producer/MovieProducer.java
+++ src/main/java/com/example/producer/MovieProducer.java
@@ -20,27 +20,30 @@ public class MovieProducer {
 	private RabbitTemplate rabbitTemplate;
 	private int countDrama = 0;
 	private int countThriller = 0;
-	
+
 	@Value("${conf-queue-exchange.exchange-movies}")
 	private String moviesExchange;
+	
+	@Value("${conf-queue-exchange-direct-retry.exchange-movies-work}")
+	private String moviesWorkExchange;
 
 	@Autowired
 	public MovieProducer(RabbitTemplate rabbitTemplate) {
 		this.rabbitTemplate = rabbitTemplate;
 	}
-	
-	//@Scheduled(fixedRate = 5000)
-	public void sendMessageDrama() {		
+
+	// @Scheduled(fixedRate = 5000)
+	public void sendMessageDrama() {
 		var movieDrama = new Movie();
 		movieDrama.setName("Movie" + countDrama++);
 		movieDrama.setGenre(Genre.DRAMA);
-		movieDrama.setYear(ThreadLocalRandom.current().nextInt(1900, 2020));		
+		movieDrama.setYear(ThreadLocalRandom.current().nextInt(1900, 2020));
 		log.info("Send movie drama {}", countDrama);
 		rabbitTemplate.convertAndSend(moviesExchange, "movie.drama", movieDrama);
 	}
-	
-	//@Scheduled(fixedRate = 5000)
-	public void sendMessageThriller() {		
+
+	// @Scheduled(fixedRate = 5000)
+	public void sendMessageThriller() {
 		var movieThriller = new Movie();
 		movieThriller.setName("Movie" + countThriller++);
 		movieThriller.setGenre(Genre.THRILLER);
@@ -48,13 +51,21 @@ public class MovieProducer {
 		log.info("Send movie thriller {}", countDrama);
 		rabbitTemplate.convertAndSend(moviesExchange, "movie.thriller", movieThriller);
 	}
-	
+
+	// @Scheduled(fixedRate = 5000)
+	public void sendMessageQueueNoConsumer() {
+		log.info("Send msg to queue without consumer");
+		rabbitTemplate.convertAndSend("q.no-consumer-available", "Message" + ThreadLocalRandom.current().nextInt(1000));
+	}
+
 	@Scheduled(fixedRate = 5000)
-	public void sendMessageQueueNoConsumer() {		
-			log.info("Send msg to queue without consumer");
-			rabbitTemplate.convertAndSend("q.no-consumer-available", "Message" + ThreadLocalRandom.current().nextInt(1000));
-		}
-	
-	
-	
+	public void sendMessageRetryDrama() {
+		var movieDrama = new Movie();
+		movieDrama.setName("Movie" + countDrama++);
+		movieDrama.setGenre(Genre.DRAMA);
+		movieDrama.setYear(ThreadLocalRandom.current().nextInt(1900, 2020));
+		log.info("Send movie retry exchange - drama {}", countDrama);
+		rabbitTemplate.convertAndSend(moviesWorkExchange, "movie.drama.work", movieDrama);
+	}
+
 }
